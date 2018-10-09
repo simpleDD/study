a5362e609be72882f33991f7f88f486ccd80c6fe
liujq <1454704370@qq.com>
2018-10-09 20:55:02 +0800

    整合GitHub

---
+++ springmvc_demo/src/main/java/com/liujq/webmvc/controller/MyController.java
@@ -0,0 +1,15 @@
+package com.liujq.webmvc.controller;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class    MyController implements Controller {
+    @Override
+    public ModelAndView handleRequest(javax.servlet.http.HttpServletRequest httpServletRequest, javax.servlet.http.HttpServletResponse httpServletResponse) {
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("hello","hellomvc");
+        modelAndView.setViewName("index");
+        return modelAndView;
+    }
+}
---
+++ springmvc_demo/src/main/webapp/WEB-INF/jsps/index.jsp
@@ -0,0 +1,6 @@
+<%@ page language="java" pageEncoding="UTF-8"%>
+<html>
+<body>
+<h2>${hello }</h2>
+</body>
+</html>
